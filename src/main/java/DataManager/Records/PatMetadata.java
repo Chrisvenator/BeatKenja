12377c5bff5859dd467478645248a8a2504ee0a5
Chrisvenator <55980003+Chrisvenator@users.noreply.github.com>
2024-02-18 22:17:18 +0100

    renamed "type" to "difficulty"

--- src/main/java/DataManager/Records/PatMetadata.java
+++ src/main/java/DataManager/Records/PatMetadata.java
@@ -3,11 +3,11 @@ package DataManager.Records;
 import java.util.ArrayList;
 import java.util.List;
 
-public record PatMetadata(double bpm, double nps, String label, List<String> tags, List<String> genre) {
+public record PatMetadata(double bpm, double nps, String difficulty, List<String> tags, List<String> genre) {
     public String toString() {
         return this.bpm + ";" +
                 this.nps + ";" +
-                this.label + ";" +
+                this.difficulty + ";" +
                 this.tags.toString().replace("[]", "NULL").replace("[", "").replace(" ", "").replaceAll("\"", "").replace("]", "") + ";" +
                 this.genre.toString().replace("[]", "NULL").replace("[", "").replace(" ", "").replaceAll("\"", "").replace("]", "") + "\n";
 
