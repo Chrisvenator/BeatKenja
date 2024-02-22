e22efadf4c425112bb95f2ab015eafe2b6701c42
Chrisvenator <55980003+Chrisvenator@users.noreply.github.com>
2024-02-22 12:14:02 +0100

    removed deprecated functions and classes "Sequence" & "Patterns"

--- src/main/java/MapGeneration/GenerationElements/Types/TemplatePattern.java
+++
@@ -1,9 +0,0 @@
-package MapGeneration.GenerationElements.Types;
-
-import MapGeneration.GenerationElements.Sequence;
-
-import java.util.List;
-
-@Deprecated
-public record TemplatePattern(String category, String genre, float nps_min, float nps_max, List<Sequence> sequences) {
-}
