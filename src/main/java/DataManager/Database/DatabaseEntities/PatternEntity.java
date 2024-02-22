a76020e818b1d3fa00fafd4bf6bcf298da43f4cc
Chrisvenator <55980003+Chrisvenator@users.noreply.github.com>
2024-02-22 13:04:38 +0100

    added tests for the EntityOperations

--- src/main/java/DataManager/Database/DatabaseEntities/PatternEntity.java
+++ src/main/java/DataManager/Database/DatabaseEntities/PatternEntity.java
@@ -3,7 +3,7 @@ package DataManager.Database.DatabaseEntities;
 import javax.persistence.*;
 
 @Entity
-@NamedQuery(name = "PatternEntity.findByPatternDescription", query = "SELECT p FROM PatternEntity p WHERE patternDescriptionId = :id")
+@NamedQuery(name = "PatternEntity.findByPatternDescriptionId", query = "SELECT p FROM PatternEntity p WHERE patternDescriptionId = :patternDescriptionId")
 @NamedQuery(name = "PatternEntity.findById", query = "SELECT p FROM PatternEntity p WHERE p.id = :id")
 @NamedQuery(name = "PatternEntity.find", query = "SELECT p FROM PatternEntity p WHERE p.patternDescriptionId = :patternDescriptionId AND p.noteId = :noteId AND p.followedByNoteId = :followedByNoteId")
 @Table(name = "pattern", schema = "beatkenja", catalog = "")
