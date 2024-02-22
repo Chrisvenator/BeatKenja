c8248b4bbc5c6401d715041f9d18a8b0c9d8a522
Chrisvenator <55980003+Chrisvenator@users.noreply.github.com>
2024-02-22 12:12:55 +0100

    migration from files to database

--- src/main/java/MapGeneration/GenerationElements/Types/TemplatePattern.java
+++ src/main/java/MapGeneration/GenerationElements/Types/TemplatePattern.java
@@ -4,5 +4,6 @@ import MapGeneration.GenerationElements.Sequence;
 
 import java.util.List;
 
+@Deprecated
 public record TemplatePattern(String category, String genre, float nps_min, float nps_max, List<Sequence> sequences) {
 }
