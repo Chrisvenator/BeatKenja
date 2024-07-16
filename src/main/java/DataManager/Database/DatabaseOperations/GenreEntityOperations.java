20e6d9a8548477de8625f38fae8005f760101b4f
Christopher Scherling <Christopher.Scherling@helvetia.at>
2024-07-16 16:55:19 +0200

    Remove dependency on database

--- src/main/java/DataManager/Database/DatabaseOperations/GenreEntityOperations.java
+++ src/main/java/DataManager/Database/DatabaseOperations/GenreEntityOperations.java
@@ -1,9 +1,11 @@
 package DataManager.Database.DatabaseOperations;
 
 import DataManager.Database.DatabaseEntities.GenreEntity;
+import DataManager.Parameters;
 
 import javax.persistence.NoResultException;
 import java.util.ArrayList;
+import java.util.List;
 
 import static DataManager.Parameters.entityManager;
 
@@ -12,13 +14,59 @@ public class GenreEntityOperations extends GenreEntity {
         return (GenreEntity) entityManager.createNamedQuery("GenreEntity.findGenre").setParameter("GenreName", GenreName).getSingleResult();
     }
 
-    public static ArrayList<GenreEntity> getAllGenres() {
+    private static ArrayList<GenreEntity> getAllGenres() {
         try {
             return (ArrayList<GenreEntity>) entityManager.createNamedQuery("GenreEntity.findAllGenres").getResultList();
         } catch (NoResultException e) {
             System.err.println("ERROR: Could not find a Genre");
             return new ArrayList<>();
         }
+    }
+
+    public static List<String> getAllGenreNames() {
+        if (!Parameters.useDatabase) {
+            List<String> genreNames = new ArrayList<>();
+            genreNames.add("Alternative");
+            genreNames.add("Ambient");
+            genreNames.add("Anime");
+            genreNames.add("Classical & Orchestral");
+            genreNames.add("Comedy & Meme");
+            genreNames.add("Dance");
+            genreNames.add("Drum and Bass");
+            genreNames.add("Dubstep");
+            genreNames.add("Electronic");
+            genreNames.add("Folk & Acoustic");
+            genreNames.add("Funk & Disco");
+            genreNames.add("Hardcore");
+            genreNames.add("Hip Hop & Rap");
+            genreNames.add("Holiday");
+            genreNames.add("House");
+            genreNames.add("Indie");
+            genreNames.add("Instrumental");
+            genreNames.add("J-Pop");
+            genreNames.add("J-Rock");
+            genreNames.add("Jazz");
+            genreNames.add("K-Pop");
+            genreNames.add("Kids & Family");
+            genreNames.add("Metal");
+            genreNames.add("Nightcore");
+            genreNames.add("Pop");
+            genreNames.add("Punk");
+            genreNames.add("R&B");
+            genreNames.add("Rock");
+            genreNames.add("Soul");
+            genreNames.add("Speedcore");
+            genreNames.add("Swing");
+            genreNames.add("TV & Film");
+            genreNames.add("Techno");
+            genreNames.add("Trance");
+            genreNames.add("Video Game");
+            genreNames.add("Vocaloid");
+
+            return genreNames;
+        }
+
+        return GenreEntityOperations.getAllGenres().stream().map(GenreEntity::getName).toList();
     }
 
     protected static GenreEntity getGenre(int fkGenreId) {
--- src/main/java/DataManager/Database/DatabaseOperations/TagEntityOperations.java
+++ src/main/java/DataManager/Database/DatabaseOperations/TagEntityOperations.java
@@ -1,9 +1,11 @@
 package DataManager.Database.DatabaseOperations;
 
 import DataManager.Database.DatabaseEntities.TagEntity;
+import DataManager.Parameters;
 
 import javax.persistence.NoResultException;
 import java.util.ArrayList;
+import java.util.List;
 
 import static DataManager.Parameters.entityManager;
 
@@ -12,13 +14,29 @@ public class TagEntityOperations extends TagEntity {
         return (TagEntity) entityManager.createNamedQuery("TagEntity.findTag").setParameter("TagName", TagName).getSingleResult();
     }
 
-    public static ArrayList<TagEntity> getAllTags() {
+    private static ArrayList<TagEntity> getAllTags() {
         try {
             return (ArrayList<TagEntity>) entityManager.createNamedQuery("TagEntity.findAllTags").getResultList();
         } catch (NoResultException e) {
             System.err.println("ERROR: Could not find a Tag");
             return new ArrayList<>();
         }
+    }
+
+    public static List<String> getAllTagNames() {
+        if (!Parameters.useDatabase){
+            List<String> l = new ArrayList<>();
+            l.add("Accuracy");
+            l.add("Balanced");
+            l.add("Challenge");
+            l.add("Dance");
+            l.add("Fitness");
+            l.add("Speed");
+            l.add("Tech");
+
+            return l;
+        }
+        return TagEntityOperations.getAllTags().stream().map(TagEntity::getName).toList();
     }
 
     public static TagEntity getTag(int fkTagId) {
