3297e5e104fd95d2aada115809917b9b9c042a35
Christopher Scherling <Christopher.Scherling@helvetia.at>
2024-08-08 15:45:59 +0200

    linear Map generation: sleep interrupted

--- src/main/java/UserInterface/Elements/Buttons/ButtonTypes/MapCreator/Button/CreateLinearMap.java
+++ src/main/java/UserInterface/Elements/Buttons/ButtonTypes/MapCreator/Button/CreateLinearMap.java
@@ -1,6 +1,7 @@
 package UserInterface.Elements.Buttons.ButtonTypes.MapCreator.Button;
 
 import BeatSaberObjects.Objects.BeatSaberMap;
+import UserInterface.Elements.Buttons.ButtonTypes.MapCreator.Exceptions.TookTooLongException;
 import UserInterface.Elements.Buttons.ButtonTypes.MapCreator.MapCreatorSubButton;
 import UserInterface.Elements.Buttons.MyButton;
 import UserInterface.Elements.ElementTypes;
@@ -8,7 +9,9 @@ import UserInterface.UserInterface;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import static DataManager.Parameters.logger;
 import static MapGeneration.PatternGeneration.LinearSlowPattern.linearSlowPattern;
 
 public class CreateLinearMap extends MapCreatorSubButton {
@@ -24,12 +27,16 @@ public class CreateLinearMap extends MapCreatorSubButton {
             UserInterface.currentDiff = uiMap.difficultyFileName;
             uiMap.toBlueLeftBottomRowDotTimings();
 
-            Thread calculateNewMap = new Thread(() -> {
-                BeatSaberMap map = new BeatSaberMap(linearSlowPattern(List.of(uiMap._notes), false, null, null));
-                maps.add(map);
-            });
+            BeatSaberMap map = null;
 
-            watchOverThread(calculateNewMap);
+            try {
+                map = runWithTimeout(() -> new BeatSaberMap(linearSlowPattern(List.of(uiMap._notes), false, null, null)), 5, TimeUnit.SECONDS);
+            } catch (Exception e) {
+                printException(new TookTooLongException("Took too long lol"));
+                logger.error("Map computation took too long! Skipping...");
+            }
+
+            maps.add(map != null ? map : uiMap);
         }
         loadNewlyCreatedMaps(maps);
     }
