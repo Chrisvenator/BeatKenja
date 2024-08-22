e021c4502f88d20a7edc444267860bdbbc6a769c
Chrisvenator <55980003+chrisvenator@users.noreply.github.com>
2024-08-22 11:06:38 +0200

    Created Super class for Plotters & Added Javadoc

--- src/main/java/MapAnalysation/PatternVisualisation/CombinedPlotter.java
+++ src/main/java/MapAnalysation/PatternVisualisation/CombinedPlotter.java
@@ -1,5 +1,6 @@
 package MapAnalysation.PatternVisualisation;
 
+import MapAnalysation.PatternVisualisation.NpsPlotters.NpsPlotter;
 import lombok.Getter;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -14,40 +15,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Combines multiple XYSeries and plots them over each other
+ * A class that combines multiple XYSeries datasets and plots them together on a single chart.
+ * This is useful for visualizing and comparing multiple data series in the same graphical context.
+ * <p>
+ * The combined plot allows for easy comparison between different datasets, making it suitable for analysis
+ * where multiple variables or metrics need to be observed together.
  */
 @Getter
-public class CombinedPlotter extends ApplicationFrame {
+public class CombinedPlotter extends NpsPlotter {
     private final List<XYSeries> seriesList;
 
+    /**
+     * A class that combines multiple XYSeries datasets and plots them together on a single chart.
+     * This is useful for visualizing and comparing multiple data series in the same graphical context.
+     * <p>
+     * The combined plot allows for easy comparison between different datasets, making it suitable for analysis
+     * where multiple variables or metrics need to be observed together.
+     */
     public CombinedPlotter(String title, XYSeries ... series) {
-        super(title);
+        super(title, title);
         seriesList = new ArrayList<>(List.of(series));
     }
 
+    /**
+     * Combines the individual XYSeries into a single dataset for plotting.
+     *
+     * @return A combined {@code XYSeriesCollection} containing all the series provided to this plotter.
+     */
     private XYSeriesCollection getCombinedDataset() {
         XYSeriesCollection dataset = new XYSeriesCollection();
         seriesList.forEach(dataset::addSeries);
         return dataset;
     }
 
-
-    public void visualize() {
-        JFreeChart xylineChart = ChartFactory.createXYLineChart(
-                super.getTitle(),
-                "Time (Seconds)",
-                "Values",
-                getCombinedDataset(),
-                PlotOrientation.VERTICAL,
-                true, true, false);
-
-        ChartPanel chartPanel = new ChartPanel(xylineChart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
-        setContentPane(chartPanel);
-
-        this.pack();
-        RefineryUtilities.centerFrameOnScreen(this);
-        this.setVisible(true);
+    @Override
+    public void visualize(XYSeries ... seriesList) {
+        super.visualize(this.seriesList.toArray(new XYSeries[0]));
     }
 }
 
--- src/main/java/MapAnalysation/PatternVisualisation/NpsPlotters/NpsPlotter.java
+++ src/main/java/MapAnalysation/PatternVisualisation/NpsPlotters/NpsPlotter.java
@@ -10,16 +10,32 @@ import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * An abstract class that serves as a base for different types of Notes Per Second (NPS) plotters.
+ * This class provides common functionality for visualizing NPS data, such as setting up the chart and displaying it in a window.
+ * Subclasses are expected to compute the NPS data and populate the `series` object with the relevant data points.
+ * <p>
+ * The visualization is created using the JFreeChart library, and it displays how the NPS varies over time.
+ * The chart is shown in a window with a default size of 800x600 pixels.
+ */
 @Getter
 public abstract class NpsPlotter extends ApplicationFrame {
     protected final XYSeries series;
     protected final String freeChartTitle;
 
+    /**
+     * Constructs an NpsPlotter with the specified title and chart title.
+     *
+     * @param title           The title of the application frame, which will also be used as the title of the window.
+     * @param freeChartTitle  The title of the chart that will be displayed on the plot.
+     */
     public NpsPlotter(String title, String freeChartTitle) {
         super(title);
         this.series = new XYSeries(super.getTitle());
         this.freeChartTitle = freeChartTitle;
-
     }
 
     /**
@@ -28,8 +44,10 @@ public abstract class NpsPlotter extends ApplicationFrame {
      * <p>
      * The chart is displayed in a window with a size of 800x600 pixels and is centered on the screen.
      */
-    public void visualize(){
+    public void visualize(XYSeries ... seriesList){
         XYSeriesCollection dataset = new XYSeriesCollection();
+
+        if (seriesList != null && seriesList.length != 0) Arrays.stream(seriesList).toList().forEach(dataset::addSeries);
         dataset.addSeries(series);
 
         JFreeChart xylineChart = ChartFactory.createXYLineChart(
